cea2c90a1d406a2ce94f8076643764a2a6a9fa03
Zachary Garwood <garwoodzachary@gmail.com>
2023-12-18 17:55:28 -0600

    Removed accidental import in day1

--- 2023/day1/trebuchet.java
+++ 2023/day1/trebuchet.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
-
-import javax.sound.midi.Soundbank;
-
 import java.util.HashMap;
 
 class Trebuchet {
