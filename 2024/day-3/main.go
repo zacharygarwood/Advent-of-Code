b48bce03495eba9c8a4b3e92cff4000e9149767c
Zachary Garwood <garwoodzachary@gmail.com>
2024-12-03 20:14:13 -0600

    Day 3 part 2

--- 2024/day-3/main.go
+++ 2024/day-3/main.go
@@ -14,16 +14,22 @@ type MulOperation struct {
 }
 
 func main() {
-	multiplications, err := parseInput("input.txt")
+	part1Multiplications, err := parseInput("input.txt", `mul\((\d{1,3}),(\d{1,3})\)`)
 	if err != nil {
 		fmt.Println("Error parsing input file:", err)
 		return
 	}
-	fmt.Printf("Part 1: %v\n", part1(multiplications))
+	part2Multiplications, err := parseInput("input.txt", `mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)`)
+	if err != nil {
+		fmt.Println("Error parsing input file:", err)
+	}
+
+	fmt.Printf("Part 1: %v\n", compute(part1Multiplications))
+	fmt.Printf("Part 2: %v\n", compute(part2Multiplications))
 	return
 }
 
-func part1(multiplications []MulOperation) int {
+func compute(multiplications []MulOperation) int {
 	sum := 0
 	for _, mul := range multiplications {
 		sum += mul.Left * mul.Right
@@ -32,31 +38,44 @@ func part1(multiplications []MulOperation) int {
 }
 
 // Returns the multiplications in format [[mul(123,456), 123, 456], [mul(111,222), 111, 222]
-func parseInput(filename string) ([]MulOperation, error) {
+// If there is a do() operation then it includes the muls else if don't() then they are excluded
+func parseInput(filename string, pattern string) ([]MulOperation, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
 	input := string(data)
-	pattern := `mul\((\d{1,3}),(\d{1,3})\)`
 	regex := regexp.MustCompile(pattern)
 	matches := regex.FindAllStringSubmatch(input, -1)
 
+	shouldInclude := true
 	var operations []MulOperation
 	for _, match := range matches {
-		left, err1 := strconv.Atoi(match[1])
-		right, err2 := strconv.Atoi(match[2])
-		if err1 != nil || err2 != nil {
-			fmt.Printf("Error converting mul values to ints left: %s, right: %s", match[1], match[2])
-			return nil, err1
+		value := match[0]
+		if value == "do()" {
+			shouldInclude = true
+			continue
 		}
-		operation := MulOperation{
-			Value: match[0],
-			Left:  left,
-			Right: right,
+		if value == "don't()" {
+			shouldInclude = false
+			continue
 		}
-		operations = append(operations, operation)
+
+		if shouldInclude {
+			left, err1 := strconv.Atoi(match[1])
+			right, err2 := strconv.Atoi(match[2])
+			if err1 != nil || err2 != nil {
+				fmt.Printf("Error converting mul values to ints left: %s, right: %s", match[1], match[2])
+				return nil, err1
+			}
+			operation := MulOperation{
+				Value: match[0],
+				Left:  left,
+				Right: right,
+			}
+			operations = append(operations, operation)
+		}
 	}
 	return operations, nil
 }
